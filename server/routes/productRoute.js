430cbd63b405cf5174ddccb2ab0d4e33f674633e
Emsi01 <h22emmru@du.se>
2024-03-11 15:32:31 +0100

    skapat routes, service och helper

---
+++ server/routes/productRoute.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const productService = require('../services/productService');
+
+
+// Hämta alla produkter
+router.get('/', (req, res) => {
+    productService.getAll().then((result) => {
+      res.status(result.status).json(result.data);
+    });
+  });
+
+// Hämta alla produkter och inkludera betyg 
+  router.get('/:id', (req, res) => {
+    productService.getAll().then((result) => {
+      res.status(result.status).json(result.data);
+    });
+  });
+
+// Ge produkt betyg 
+  router.post('/:id/addRating', (req, res) => {
+    productService.getAll().then((result) => {
+      res.status(result.status).json(result.data);
+    });
+  });
+
+// Nedan följer crud funktioner för produkt 
+  router.post('/:id', (req, res) => {
+    const post = req.body;
+    productService.create(post).then((result) => {
+    res.status(result.status).json(result.data);
+    });
+  });
+
+  router.put('/', (req, res) => {
+    const post = req.body;
+    const id = post.id;
+
+  productService.update(post, id).then((result) => {
+    res.status(result.status).json(result.data);
+    });
+  });
+
+  router.delete('/', (req, res) => {
+    const id = req.body.id;
+  productService.destroy(id).then((result) => {
+    res.status(result.status).json(result.data);
+    });
+  });
---
+++ server/services/productService.js
@@ -0,0 +1,18 @@
+const db = require('../models');
+const {
+  createResponseSuccess,
+  createResponseError,
+  createResponseMessage
+} = require('../helpers/responseHelper');
+const validate = require('validate.js');
+
+
+async function getAll() {
+
+    try {
+      const allProducts = await db.products.findAll({ include: [db.users, db.ratings] });
+      return createResponseSuccess(allProducts.map((products) => _formatPost(products)));
+    } catch (error) {
+      return createResponseError(error.status, error.message);
+    }
+  }
