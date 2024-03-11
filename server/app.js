fd19b25d53298d8d82b6408edc5e7be20064fb45
Emsi01 <h22emmru@du.se>
2024-03-11 11:05:18 +0100

    Co-authored-by: h22oloer <h22oloer@users.noreply.github.com> Co-authored-by: Dennis Andersson (Student) <h22dande@du.se> Co-authored-by: Marcus Schönning (Student) <h22mscho@du.se> // Börja på databas

--- server/app.js
+++ server/app.js
@@ -1,10 +1,6 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -12,9 +8,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
+    next();
+});
+app.use("/posts", require("./routes/postsRoute"));
+app.use("/users", require("./routes/usersRoute"));
+app.use("/tags", require("./routes/tagsRoute"));
 
 module.exports = app;
